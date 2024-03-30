5ae7b6b34ae8ad1ee843e2d5087d351bb1f07b8c
mdaftab41 <mdaftabalam49041@gmail.com>
2024-03-31 01:08:32 +0530

    Mathin javascript

--- basicJavaScript/basic/string.js
+++ basicJavaScript/basic/string.js
@@ -17,3 +17,19 @@ const s ="  aftab alam ";
 console.log(s);
 console.log(s.trim());// remove first and last whitespace
 console.log(s.replace(" " , "/")) // remove (first sting) whitespace by /
+
+// math in javav script 
+console.log(Math);
+console.log(Math.abs(-8));
+console.log(Math.round(6.46));
+console.log(Math.floor(6.64));
+console.log(Math.ceil(4.94));
+console.log(Math.max(4,5,6));
+console.log(Math.min(4,5,6));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+// if oyu want value b/w 10 and 20
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min))+min);
