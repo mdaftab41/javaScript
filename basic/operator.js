83bdcbf598082704dc90d4bf45e030214d051829
mdaftab41 <mdaftabalam49041@gmail.com>
2024-03-30 17:50:46 +0530

    operator

---
+++ basic/operator.js
@@ -0,0 +1,17 @@
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)//power
+console.log(2/2)
+console.log(2%2)//remainder
+
+let str1="aftab"
+let str2="alam"
+let str3 =str1+str2;
+console.log(str3);// aftabalam
+
+console.log("2"+ 1) // both as sting 21
+console.log(2 +"1") //21
+console.log(2+2 +"1")//41
+console.log("1" + 2+2+2)// 1222
+console.log(+true)//  1
--- key.js
+++ key.js
@@ -3,3 +3,4 @@ const acc=13456;
 var id=" alam@.com";
 let state= "bihar";
 console.table([acc, id, state]);
+ 
