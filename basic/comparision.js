e09ab2b0ffdc160b81191dce8946823dc7e019b5
mdaftab41 <mdaftabalam49041@gmail.com>
2024-03-30 18:13:52 +0530

    change in location

---
+++ basic/comparision.js
@@ -0,0 +1,13 @@
+// only compare the value not compare the data type
+console.log(3>4)
+console.log(3>=4)
+console.log(3<4)
+console.log(3<=4)
+console.log(3==4)
+console.log(null<0)
+console.log(null>0)
+console.log(null==0)
+console.log(undefined<0)
+console.log(undefined>0)
+console.log(undefined=0)
+// strick cheak cheak both data and datatype 
