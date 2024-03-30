5616b05238da2131f8632b1b55962ca88b0d61fe
mdaftab41 <mdaftabalam49041@gmail.com>
2024-03-30 22:30:21 +0530

    change in location

---
+++ basicJavaScript/basic/string.js
@@ -0,0 +1,19 @@
+const name =" aftab"
+const repoCount =50;
+console.log(name+" " +repoCount +" value");
+// we use backtik 
+console.log(`Hello my name is${name} and my repo count is${repoCount}`);
+
+const newString = new String("Aftabalam");
+console.log(newString);
+console.log(newString.toLowerCase());
+console.log(newString.toUpperCase());
+console.log(newString.slice(-8, 4));
+console.log(newString.substring(0, 4));
+console.log(newString.charAt(5));
+console.log(newString.indexOf('a'));//give firt index
+
+const s ="  aftab alam ";
+console.log(s);
+console.log(s.trim());// remove first and last whitespace
+console.log(s.replace(" " , "/")) // remove (first sting) whitespace by /
