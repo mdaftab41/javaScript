10e465e5f23f3d94d9c722eece7a664b448a0308
mdaftab41 <mdaftabalam49041@gmail.com>
2024-03-30 17:20:06 +0530

    datatype

---
+++ DataType.js
@@ -0,0 +1,12 @@
+"use strict" // trea all js  code newer version
+//alert(3+3)  // we are using noddejs not browser
+
+let name= "string"  // string data type
+let age=274;// number 2 to power of 53
+let isLoggedIn = false;// boolean  true or false 
+let ans ; //undefine  data type 
+// symbol 
+// null 
+// bigint 
+console.log(typeof undefined)// undefine
+console.log(typeof null) //object 
