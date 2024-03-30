f76081e34d5aa16ae494d91163558af812aa2cf0
mdaftab41 <mdaftabalam49041@gmail.com>
2024-03-31 01:28:02 +0530

    date

---
+++ basicJavaScript/basic/Date.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleString());
+console.log(myDate.getDate());
+console.log(myDate.getTime());
+console.log(myDate.getMinutes());
+console.log(myDate.getHours());
+let myDat = new Date(2023, 0, 23)
+console.log(myDat)
--- basicJavaScript/basic/Number.js
+++ basicJavaScript/basic/Number.js
@@ -9,3 +9,19 @@
  const num1 = new Number(276396000);
  console.log(num1.toLocaleString())// place comman in international place value chart  us standard
  console.log(num1.toLocaleString("en-IN"))// indian standard
+ // math in javav script 
+console.log(Math);
+console.log(Math.abs(-8));
+console.log(Math.round(6.46));
+console.log(Math.floor(6.64));
+console.log(Math.ceil(4.94));
+console.log(Math.max(4,5,6));
+console.log(Math.min(4,5,6));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+// if oyu want value b/w 10 and 20
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min))+min);
+ 
