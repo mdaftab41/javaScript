28a69a9fefcd6d67923aef8bd34a66b63b5de42c
mdaftab41 <mdaftabalam49041@gmail.com>
2024-04-03 13:13:56 +0530

    guessGame

---
+++ GessNumber/index.js
@@ -0,0 +1,36 @@
+const randomNumber = (parseInt((Math.random()*10)+1))  //use to generate a random number
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses')
+const remaning = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultparas');
+const p =document.createElement('p');
+
+let preGuess=[]//empty array to store user guess value
+let numGuess= 1;//default value
+let playGame= true;
+
+function validateGuess(guess){
+  //  
+}
+
+function cheackGuess(guess){
+    //
+}
+
+function displayGuess(guess){
+    //
+}
+
+function displayMassage(massage){
+    //
+}
+
+function endGame(){
+    //
+}
+
+function newGame(){
+    //
+}
