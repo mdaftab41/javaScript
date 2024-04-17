ced863dc9a8ad0139372828500a35ed838a052ed
mdaftab41 <mdaftabalam49041@gmail.com>
2024-04-18 00:16:12 +0530

    catah()

--- Advance-js/API-js/api.js
+++ Advance-js/API-js/api.js
@@ -6,7 +6,22 @@ async function getAllUser() {
     const data = await response.json(); // convert response into json formate it take time so we use await here
     console.log(data);
   } catch (error) {
-    console.log(error);
+    console.log("E: " ,error);
   }
 }
 getAllUser();
+
+// we also use .then() .catch()
+
+console .log("we also use .then() .catch() is start")
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((respose)=>{
+    return respose.json()
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((error)=>{
+    console.log(error);
+})
