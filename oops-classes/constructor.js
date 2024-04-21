90f90d2f99dbfb08fc313784b6b41f20c26d30fb
mdaftab41 <mdaftabalam49041@gmail.com>
2024-04-22 01:09:14 +0530

    example code

--- oops-classes/constructor.js
+++ oops-classes/constructor.js
@@ -94,3 +94,40 @@ function Car(make, model, year) {
   console.log(dog instanceof Object);  // true, because all objects inherit from Object
   console.log(dog instanceof Array);   // false
   
+
+
+  // new example from mixBlendMode: 
+  class Polygon {
+    constructor() {
+      this.name = "Polygon";
+    }
+  }
+  
+  class Rectangle {
+    constructor() {
+      this.name = "Rectangle";
+    }
+  }
+  
+  class Square extends Polygon {
+    constructor() {
+      super();
+    }
+  }
+  
+  // Make Square extend Rectangle (which is a base class) instead of Polygon
+  Object.setPrototypeOf(Square, Rectangle);
+  
+  const newInstance = new Square();
+  
+  // newInstance is still an instance of Polygon, because we didn't
+  // change the prototype of Square.prototype, so the prototype chain
+  // of newInstance is still
+  //   newInstance --> Square.prototype --> Polygon.prototype
+  console.log(newInstance instanceof Polygon); // true
+  console.log(newInstance instanceof Rectangle); // false
+  
+  // However, because super() calls Rectangle as constructor, the name property
+  // of newInstance is initialized with the logic in Rectangle
+  console.log(newInstance.name); // Rectangle
+  
