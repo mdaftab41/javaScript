15fd43536d7185dc0c421b8a242063863747da93
mdaftab41 <mdaftabalam49041@gmail.com>
2024-04-21 23:49:44 +0530

    instanceOf

--- oops-classes/constructor.js
+++ oops-classes/constructor.js
@@ -58,3 +58,39 @@ const user3 = new userDetail("Md Aftab alam", 13,true)
 const user4 = new userDetail("Parshant Malik" , 11, false)
 console.log(user3.constructor) // constructor is reference of itself
 console.log(user4)
+
+//In JavaScript, the instanceof operator is used to check whether an object is an instance of a particular class or constructor function.
+// It returns true if the object is an instance of the specified constructor, otherwise, it returns false.
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  const auto = new Car('Honda', 'Accord', 1998);
+  
+  console.log(auto instanceof Car);
+  // Expected output: true
+  
+  console.log(auto instanceof Object);
+
+
+  class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name, breed) {
+      super(name);
+      this.breed = breed;
+    }
+  }
+  
+  let dog = new Dog('Buddy', 'Golden Retriever');
+  
+  console.log(dog instanceof Dog);     // true
+  console.log(dog instanceof Animal);  // true, because Dog extends Animal
+  console.log(dog instanceof Object);  // true, because all objects inherit from Object
+  console.log(dog instanceof Array);   // false
+  
