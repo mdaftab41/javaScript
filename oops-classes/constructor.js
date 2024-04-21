0795e93f31f01dec12c0e0220485d4c130b675f0
mdaftab41 <mdaftabalam49041@gmail.com>
2024-04-21 23:38:34 +0530

    constructer

--- oops-classes/constructor.js
+++ oops-classes/constructor.js
@@ -42,3 +42,19 @@ const userOne = new User("Alam", 13,true)
 const user2 = new User("Aftab" , 11, false)
 console.log(userOne)
 console.log(user2)
+
+// we also write a function and methode 
+function userDetail( username , loginCount , isLoggedIn ){
+    this.username= username ,
+    this.loginCount = loginCount,
+    this.isLoggedIn = isLoggedIn,
+    this.greeting = function(){
+        console.log(`welcome ${this.username}`)
+    }
+    // we return the this to pass object if you not pass this then it pass a empty object
+    return this
+}
+const user3 = new userDetail("Md Aftab alam", 13,true)
+const user4 = new userDetail("Parshant Malik" , 11, false)
+console.log(user3.constructor) // constructor is reference of itself
+console.log(user4)
