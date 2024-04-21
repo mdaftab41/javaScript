778c11a7d82d235c16f2627cb744c0bbc6a47dab
mdaftab41 <mdaftabalam49041@gmail.com>
2024-04-21 16:23:51 +0530

    complete

--- oops-classes/objectLiteral.js
+++ oops-classes/objectLiteral.js
@@ -14,6 +14,26 @@ const user = {
     }
 
 }
+const user2 = {
+    username : " alam",
+    signedIn : false,
+    loginCount : 5 ,
+    getUserDetail: function (){
+    console.log("Got user details from databs");
+    // this keyword: we use this in current context 
+    console.log(`Username: ${this.username}`)
+   // printing current context 
+   console.log( "current context")
+    console.log(this)
+
+
+    }
+
+}
+console.log(user.username);
+console.log(user.getUserDetail());
+// this in global context 
+console.log(this)
 console.log(user.username);
 console.log(user.getUserDetail());
 // this in global context 
