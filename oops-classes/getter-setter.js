5d4fe94f64cc19b2bbeda12bf5e065362e947c9f
mdaftab41 <mdaftabalam49041@gmail.com>
2024-04-24 01:01:46 +0530

    complete

--- oops-classes/getter-setter.js
+++ oops-classes/getter-setter.js
@@ -25,7 +25,51 @@ class User{
         this._password=value 
         //this._password=value.toUpperCase()
     }
+    get email(){
+       return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email=value
+    }
 }
 const alam = new User("Aftab Alam" , "Alam@gmail.com","asgreg")
 console.log(alam.password)
 console.log(alam)
+console.log(alam.email)
+
+//code for circle
+
+class Circle {
+    constructor(radius) {
+      this._radius = radius;
+    }
+  
+    // Getter
+    get radius() {
+      return this._radius;
+    }
+  
+    // Setter
+    set radius(value) {
+      if (value <= 0) {
+        throw new Error('Radius must be positive');
+      }
+      this._radius = value;
+    }
+  
+    // Getter for area
+    get area() {
+      return Math.PI * this._radius ** 2;
+    }
+  }
+  
+  const myCircle = new Circle(5);
+  console.log(myCircle.radius); // Output: 5
+  console.log(myCircle.area); // Output: 78.53981633974483
+  
+  myCircle.radius = 10; // Using the setter
+  console.log(myCircle.radius); // Output: 10
+  console.log(myCircle.area); // Output: 314.1592653589793
+  
+  myCircle.radius = -5; // This will throw an error from the setter
+  
