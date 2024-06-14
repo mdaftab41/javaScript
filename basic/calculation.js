f39006d9e8a86aec2306c26f19091223e066b4f2
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-14 23:31:41 +0530

    basic

---
+++ basic/calculation.js
@@ -0,0 +1,29 @@
+let a=67;
+console.log(a);
+console.log("hello bro")
+console.log(7+6+a)
+let username= "aftab"
+console.log(username) 
+let radious=5
+//let pi=3.14
+const pi=3.14
+let area 
+area=pi*radious*radious
+console.log(area)
+/*you can change the value variable*/
+radious=6
+area=pi*radious*radious
+console.log(area)
+/*you can't change the value of constant*/
+radious=6
+//pi=4.3
+area=pi*radious*radious
+console.log(area)
+/*data type*/
+let num=12e1
+console.log(typeof num,num)
+/*string */
+/*let user="aftab\n alam"
+console.log(user)*/
+let user="aftab\t alam \v md"
+console.log(user)
