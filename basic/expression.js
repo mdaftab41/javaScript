b4e802536c7ec7006aaeb526032b3183190c089d
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-18 23:00:29 +0530

    expression

---
+++ basic/expression.js
@@ -0,0 +1,13 @@
+var x = 5;
+var y = 10;
+console.log(x > 0 && y > 0);
+var a = 15;
+var b = 0;
+console.log(a > 10 || b > 10);
+var isOpen = false;
+console.log(!isOpen);
+console.log("5" - 3);
+
+console.log(2 < 12 < 5);
+
+console.log("20" + 10 + 10);
