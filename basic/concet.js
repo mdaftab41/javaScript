566aa8db95ed4726b965c2646a6c85907fd8a0c9
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-18 22:55:29 +0530

    concatenation

---
+++ basic/concet.js
@@ -0,0 +1,10 @@
+console.log(10 + "20");
+console.log(9 - "5");
+console.log("Java" + "Script");
+console.log(" " + " ");
+console.log(" " + 0);
+console.log("vinod" - "thapa");
+console.log(true + true);
+console.log(true + false);
+console.log(false + true);
+console.log(false - true);
