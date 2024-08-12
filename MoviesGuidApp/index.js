8ca44b42e5204531a6471fab2d62ba7f07637bce
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-12 22:22:55 +0530

    MovieApp

--- MoviesGuidApp/index.js
+++ MoviesGuidApp/index.js
@@ -7,11 +7,41 @@ const getMovieInfo = async (movie)=>{
     const url =`http://www.omdbapi.com/?i=tt3896198&apikey=${myApiKey}&t=${movie}`;
     const response = await fetch(url) ;
     const data =  await response.json();
-    console.log(data) ;
+    
+    showMovieData(data);
 }  
 
-const showMoveDetail =()=>{
+const showMovieData=(data)=>{
+    movieContainer.innerHTML="";
+    movieContainer.classList.remove('noBackground');
+    //arrra Destructing
+    const {Title, imdbRating, Genre, Released ,RunTime ,Actors, Plot,Poster} = data;
+    const movieElement = document.createElement('div');
+    movieElement.classList.add('movie-info');
+    movieElement.innerHTML=`<h2>${Title}</h2>
+    <p> <strong>Rating: &#11088;</strong>${ imdbRating}</p> `;
     
+    
+    const movieGenreElement = document.createElement('div');
+    movieGenreElement.classList.add("movie-genre");
+    //Genre is astring like array (commmedy , action , love)
+    Genre.split(",").forEach(element => {
+        const p=document.createElement('p');//write the movies type
+        p.innerHTML=element ;
+        movieGenreElement.appendChild(p);
+    });
+    movieElement.appendChild(movieGenreElement);
+    //we use + to append otherwise is replase 
+    movieElement.innerHTML+=` <p> <strong>Released:</strong>${Released}</p>
+    <p> <strong>Duration:</strong>${RunTime}</p>
+    <p> <strong>Cast:</strong>${Actors}</p>
+    <p> <strong>Plot:</strong>${Plot}</p> `;
+    const moviePoster=document.createElement('div');
+    moviePoster.classList.add('movie-poster');
+    moviePoster.innerHTML = `<img src="${Poster}" alt="Poster">`;
+    movieContainer.appendChild(moviePoster);
+    movieContainer.appendChild(movieElement);
+
 }
 
 
@@ -20,5 +50,8 @@ searchForm.addEventListener('submit' , (e)=>{
      const movieName = inputBox.value.trim();
      if(movieName !=""){
         getMovieInfo(movieName) ;
+     }else{
+        movieContainer.innerHTML=`<h2>Enter Valid movie name to get movie information</h2>`
+        movieContainer.classList.add('noBackground');
      }
 });
