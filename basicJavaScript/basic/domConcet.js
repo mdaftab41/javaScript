2ffe70c68f0057c299262f27a0e99e03760aaae4
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-14 23:47:59 +0530

    Dom

--- basicJavaScript/basic/domConcet.js
+++ basicJavaScript/basic/domConcet.js
@@ -24,3 +24,10 @@ function greet(event) {
 }
 
 btn.onclick = greet;
+const btn = document.querySelector("button");
+
+function greet(event) {
+  console.log("greet:", event);
+}
+
+btn.addEventListener("click", greet);
