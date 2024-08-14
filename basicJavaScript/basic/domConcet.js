0c132e0a0488e830432290fea9ecd9b85420f0fc
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-14 23:49:01 +0530

    Dom

--- basicJavaScript/basic/domConcet.js
+++ basicJavaScript/basic/domConcet.js
@@ -31,3 +31,16 @@ function greet(event) {
 }
 
 btn.addEventListener("click", greet);
+
+
+
+const btn = document.querySelector("button");
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+btn.addEventListener("click", () => {
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+});
