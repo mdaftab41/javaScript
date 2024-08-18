19532d74337ef462acf5034ebbf1d3204e2ee2b4
mdaftab41 <mdaftabalam49041@gmail.com>
2024-08-18 23:34:56 +0530

    map

--- basic/SpecialFunction.js
+++ basic/SpecialFunction.js
@@ -4,3 +4,28 @@ const array1 = [1, 4, 9, 16];
 const map1 = array1.map((x) => x * 2);
 
 console.log(map1);
+
+
+const numbers = [3, -1, 1, 4, 1, 5, 9, 2, 6];
+const averaged = numbers
+  .filter((num) => num > 0)
+  .map((num, idx, arr) => {
+    // Without the arr argument, there's no way to easily access the
+    // intermediate array without saving it to a variable.
+    const prev = arr[idx - 1];
+    const next = arr[idx + 1];
+    let count = 1;
+    let total = num;
+    if (prev !== undefined) {
+      count++;
+      total += prev;
+    }
+    if (next !== undefined) {
+      count++;
+      total += next;
+    }
+    const average = total / count;
+    // Keep two decimal places
+    return Math.round(average * 100) / 100;
+  });
+console.log(averaged); // [2, 2.67, 2, 3.33, 5, 5.33, 5.67, 4]
