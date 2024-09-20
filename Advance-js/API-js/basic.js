2e9b93daf8db23ceb21fceb4193eaba09d9274c6
mdaftab41 <mdaftabalam49041@gmail.com>
2024-09-20 23:47:25 +0530

    API

--- Advance-js/API-js/basic.js
+++ Advance-js/API-js/basic.js
@@ -27,3 +27,24 @@ contactForm.addEventListener('submit', function (event) {
       console.error('Error:', error);
     });
 });
+
+
+// Define the API URL
+const apiUrl1 = 'https://api.example.com/data';
+
+// Make a GET request
+fetch(apiUrl1)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
