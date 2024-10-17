290f9d7b0646c4dc35a27c1371c9e08c3aa0fb97
mdaftab41 <mdaftabalam49041@gmail.com>
2024-10-17 23:28:38 +0530

    NodeConcept

--- Event in js/node.js
+++ Event in js/node.js
@@ -19,3 +19,35 @@ function eachNode(rootNode, callback) {
       }
     }
   }
+
+
+  //new  concept
+  function grep(parentNode, pattern) {
+    let matches = [];
+    let endScan = false;
+  
+    eachNode(parentNode, (node) => {
+      if (endScan) {
+        return false;
+      }
+  
+      // Ignore anything which isn't a text node
+      if (node.nodeType !== Node.TEXT_NODE) {
+        return;
+      }
+  
+      if (typeof pattern === "string" && node.textContent.includes(pattern)) {
+        matches.push(node);
+      } else if (pattern.test(node.textContent)) {
+        if (!pattern.global) {
+          endScan = true;
+          matches = node;
+        } else {
+          matches.push(node);
+        }
+      }
+    });
+  
+    return matches;
+  }
+  
